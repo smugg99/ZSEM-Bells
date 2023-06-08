626773d0daa3499bc523833a785c088d3f67e595
Dudus Jestem <dudak799@gmail.com>
2023-06-09 01:05:35 +0200

    Removed playwright
    it was actually not needed

--- classes/schedule_keeper.py
+++ classes/schedule_keeper.py
@@ -2,10 +2,11 @@ import json
 import requests
 from urllib.parse import urlparse, parse_qs
 from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
 
 import config
 import utils
+
+# note for tommorow, optimise this
 
 # ================# Functions #================ #
 
@@ -27,13 +28,12 @@ def _extract_hour_ranges(page_content: str):
     hour_ranges: list = []
     _hour_ranges: list = []
     for table_row in table_rows:
-        _hour_ranges = table_row.find_all(
-            "td", class_=config.SCHEDULE_TABLE_HOUR_CLASS_NAME)
+        _hour_ranges.append(table_row.find(
+            "td", class_=config.SCHEDULE_TABLE_HOUR_CLASS_NAME))
 
     for hour_range in _hour_ranges:
         hour_ranges.append(_parse_hour_range(hour_range.text))
 
-    print(hour_ranges)
     return hour_ranges
 
 
@@ -42,68 +42,57 @@ def _get_valid_branches():
     branch_index: int = 1
     bad_branches_count: int = 0
 
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch()
+    utils.logger.info(
+        "Getting valid branches from base url of: " + config.SCHEDULE_URL)
 
-        utils.logger.info(
-            "Getting valid branches from base url of: " + config.SCHEDULE_URL)
+    try:
+        while True:
+            branch_url: str = utils.build_branch_url(branch_index)
 
-        try:
-            while True:
-                branch_url: str = utils.build_branch_url(branch_index)
+            try:
+                response = requests.get(
+                    branch_url, timeout=config.SCHEDULE_REQUEST_TIMEOUT)
+                response.raise_for_status()
+            except Exception as e:
+                utils.logger.warn(
+                    "Failed to get response from branch of index: " + str(branch_index) + "\n" + str(e))
+                bad_branches_count += 1
+            else:
+                parsed_url: str = urlparse(response.request.path_url)
+                query_params: dict = parse_qs(parsed_url.query)
 
-                try:
-                    page = browser.new_page()
-                    page.goto(branch_url)
-                    page_content = page.content()
-                    page.screenshot(path=f'example.png')
-                    response = requests.get(
-                        branch_url, timeout=config.SCHEDULE_REQUEST_TIMEOUT)
-                    response.raise_for_status()
-                except Exception as e:
-                    utils.logger.warn(
-                        "Failed to get response from branch of index: " + str(branch_index) + "\n" + str(e))
-                    bad_branches_count += 1
+                # "error" query parameter is specific for zsem.edu.pl
+                if ("error" not in query_params) and response.status_code == 200:
+                    hour_ranges = _extract_hour_ranges(response.content)
+
+                    # Ignore the branch if it has no hour ranges
+                    if len(hour_ranges) <= 0:
+                        continue
+
+                    valid_branches.append([
+                        branch_index, hour_ranges
+                    ])
+
+                    utils.logger.info(
+                        "Got response from a valid branch of index: " + str(branch_index) + " : " + str(response.status_code))
                 else:
-                    parsed_url: str = urlparse(response.request.path_url)
-                    query_params: dict = parse_qs(parsed_url.query)
+                    utils.logger.warn(
+                        "Response returned an error query parameter: " + str(query_params))
+                    bad_branches_count += 1
 
-                    print(response.content)
+            branch_index += 1
 
-                    # "error" query parameter is specific for zsem.edu.pl
-                    if ("error" not in query_params) and response.status_code == 200:
-                        hour_ranges = _extract_hour_ranges(page_content)
+            if bad_branches_count >= config.SCHEDULE_MAX_BAD_BRANCHES:
+                utils.logger.warning(
+                    "Max number of bad branches reached: " + str(bad_branches_count))
+                break
+    except Exception as e:
+        utils.logger.error("Failed to get valid branches: " + str(e))
 
-                        # Ignore the branch if it has no hour ranges
-                        if len(hour_ranges) <= 0:
-                            continue
+    utils.logger.info("Number of valid branches: " +
+                      str(len(valid_branches)) + " : " + str(valid_branches))
 
-                        valid_branches.append([
-                            branch_index, hour_ranges
-                        ])
-
-                        utils.logger.info(
-                            "Got response from a valid branch of index: " + str(branch_index) + " : " + str(response.status_code))
-                    else:
-                        utils.logger.warn(
-                            "Response returned an error query parameter: " + str(query_params))
-                        bad_branches_count += 1
-
-                branch_index += 1
-
-                if bad_branches_count >= config.SCHEDULE_MAX_BAD_BRANCHES:
-                    utils.logger.warning(
-                        "Max number of bad branches reached: " + str(bad_branches_count))
-                    break
-        except Exception as e:
-            utils.logger.error("Failed to get valid branches: " + str(e))
-
-        utils.logger.info("Number of valid branches: " +
-                          str(len(valid_branches)) + " : " + str(valid_branches))
-
-        browser.close()
-
-        return valid_branches
+    return valid_branches
 
 # ================# Functions #================ #
 
@@ -121,6 +110,14 @@ class ScheduleKeeper():
         if utils.check_website_status(config.MAIN_SITE):
             self.valid_branches = _get_valid_branches()
 
+            longest_branch = None
+            for branch in self.valid_branches:
+                if longest_branch == None:
+                    longest_branch = branch
+                    continue
+                elif len(longest_branch[1]) < len(branch[1]):
+                    longest_branch = branch
+            print(longest_branch)
         else:
             utils.logger.error("Can't sync schedule due to " +
                                config.MAIN_SITE + " being down, attempting to use the cached one...")
