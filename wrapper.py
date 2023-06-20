fbc6d4b78db63299fb05883f7d81d33c813cab0a
Dudus Jestem <dudak799@gmail.com>
2023-06-21 01:39:29 +0200

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -19,34 +19,38 @@ def play_wav_file(file_path):
 	subprocess.run(command)
 
 def setup_gpio():
-	utils.logging_formatter.separator("Setting up GPIO")
-	gpio_pins_disabled: Optional[bool] = utils.user_config.get("disable_gpio_pins", False)	
-
-	if not gpio_pins_disabled:
-		gpio_pins_config: Optional[Dict[str, int]] = utils.user_config.get("gpio_pins", {})
+	GPIO.setmode(GPIO.BOARD)
+ 
+	GPIO.setup(12, GPIO.OUT, GPIO.LOW)
 	
-		if not gpio_pins_config:
-			utils.logger.warn("GPIO config is empty")
-		else:
-			GPIO.setmode(GPIO.BOARD)
+	# utils.logging_formatter.separator("Setting up GPIO")
+	# gpio_pins_disabled: Optional[bool] = utils.user_config.get("disable_gpio_pins", False)	
 
-			pins_to_setup = [
-				gpio_pins_config["neutral_callback"],
-				gpio_pins_config["work_callback"],
-				gpio_pins_config["break_callback"]
-			]
+	# if not gpio_pins_disabled:
+	# 	gpio_pins_config: Optional[Dict[str, int]] = utils.user_config.get("gpio_pins", {})
+	
+	# 	if not gpio_pins_config:
+	# 		utils.logger.warn("GPIO config is empty")
+	# 	else:
+	# 		GPIO.setmode(GPIO.BOARD)
 
-			for pin in pins_to_setup:
-				print(pin)
-				try:
-					GPIO.setup(int(pin), GPIO.OUT, GPIO.LOW)
-				except Exception as e:
-					utils.logger.error("GPIO pins are probably not supported on this device: " + str(e))
-					break
-				else:
-					utils.logger.info("Setting pin " + str(pin) + " as output")
-	else:
-		utils.logger.warn("GPIOs are disabled")
+	# 		pins_to_setup = [
+	# 			gpio_pins_config["neutral_callback"],
+	# 			gpio_pins_config["work_callback"],
+	# 			gpio_pins_config["break_callback"]
+	# 		]
+
+	# 		for pin in pins_to_setup:
+	# 			print(pin)
+	# 			try:
+	# 				GPIO.setup(int(pin), GPIO.OUT, GPIO.LOW)
+	# 			except Exception as e:
+	# 				utils.logger.error("GPIO pins are probably not supported on this device: " + str(e))
+	# 				break
+	# 			else:
+	# 				utils.logger.info("Setting pin " + str(pin) + " as output")
+	# else:
+	# 	utils.logger.warn("GPIOs are disabled")
 
 def cleanup_gpio():
 	gpio_pins_disabled: Optional[bool] = utils.user_config.get("disable_gpio_pins", False)	
