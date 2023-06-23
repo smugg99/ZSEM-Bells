aa397e04cd8b83372f0b5c032aa61050e8f55b84
Dudus Jestem <dudak799@gmail.com>
2023-06-23 21:26:54 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,12 +3,13 @@
 import OPi.GPIO as GPIO
 from time import sleep
 
-pin: int = 26
+pin: int = 11
 GPIO.setboard(GPIO.H616)
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(pin, GPIO.OUT)
-
+GPIO.output(pin, GPIO.HIGH)
+sleep(1)
 while True:
     GPIO.output(pin, GPIO.LOW)
     sleep(1)
