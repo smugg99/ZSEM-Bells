b12fcb42b198106f4403c34c9627f2cc02173c46
Dudus Jestem <dudak799@gmail.com>
2023-09-04 23:31:39 +0200

    Update sound_tester.py

--- src/sound_tester.py
+++ src/sound_tester.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-from pydub import AudioSegment
-from pydub.playback import play
 import os
 
 # Set the audio player command (use 'aplay' or another player if preferred)
@@ -16,15 +14,13 @@ project_root = os.path.abspath(os.path.join(script_dir, ".."))
 # Construct the path to the WAV file in the sounds folder
 wav_file_path = os.path.join(project_root, "sounds", "bell_sound.wav")
 
-# Load the WAV file
-sound = AudioSegment.from_file(wav_file_path)
+# Define the audio device you want to use (replace "hw:0,0" with the appropriate device identifier)
+audio_device = "hw:0,0"
 
-# Set the volume (0.0 to 1.0, adjust as needed)
-volume = 0.5
-sound = sound - 30 * (1 - volume)  # Adjust volume level
+# Create the aplay command with the specified audio device
+aplay_command = f"aplay -D {audio_device} {wav_file_path}"
 
-# Play the modified audio using 'aplay' or the chosen player
-sound.export(os.path.join(script_dir, "temp.wav"), format="wav")  # Export the audio to a temporary file
-os.system(f"{pydub_audio_player} {os.path.join(script_dir, 'temp.wav')}")  # Play the temporary file
+# Play the WAV file using aplay
+os.system(aplay_command)
 
 print("deezx")
