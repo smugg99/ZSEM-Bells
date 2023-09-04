06f5b4b312e9ba3f0374cac7fdf6d35a54110b57
Dudus Jestem <dudak799@gmail.com>
2023-09-04 21:05:24 +0200

    Update gpio_tester.py

--- src/gpio_tester.py
+++ src/gpio_tester.py
@@ -5,18 +5,20 @@ import OPi.GPIO as GPIO
 
 
 def main():
-	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(12, GPIO.OUT, GPIO.HIGH)
-	
-	while True:
-		sleep(1)
-		GPIO.output(12, GPIO.LOW)
-		print("Relay on")
-  
-		sleep(1)
-		GPIO.output(12, GPIO.HIGH)
-		print("Relay off")
-	
+    GPIO.setboard(GPIO.H616)
+    GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(12, GPIO.OUT, GPIO.HIGH)
+
+    while True:
+        sleep(1)
+        GPIO.output(12, GPIO.LOW)
+        print("Relay on")
+
+        sleep(1)
+        GPIO.output(12, GPIO.HIGH)
+        print("Relay off")
+
 
 if __name__ == "__main__":
-	main()
+    main()
