2e28501dd25a6f8bc17da48cc811f8254c1c7824
Dudus Jestem <dudak799@gmail.com>
2023-09-04 23:22:21 +0200

    Update sound_tester.py

--- src/sound_tester.py
+++ src/sound_tester.py
@@ -2,9 +2,19 @@
 
 from pydub import AudioSegment
 from pydub.playback import play
+import os
+
+# Get the parent directory of the script (where your script is located)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Go up two levels to reach the project root directory
+project_root = os.path.abspath(os.path.join(script_dir, ".."))
+
+# Construct the path to the WAV file in the sounds folder
+wav_file_path = os.path.join(project_root, "sounds", "bell_sound.wav")
 
 # Load the WAV file
-sound = AudioSegment.from_file("/ZSEM-Bells/sounds/bell_sound.wav")
+sound = AudioSegment.from_file(wav_file_path)
 
 # Set the volume (0.0 to 1.0, adjust as needed)
 volume = 0.5
