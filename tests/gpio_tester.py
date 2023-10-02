32a2cca89403cc68f0696ac04f7102a580413e48
SmeggMann99 <dudak799@gmail.com>
2023-10-02 15:15:17 +0200

    Update gpio_tester.py

--- tests/gpio_tester.py
+++ tests/gpio_tester.py
@@ -6,15 +6,15 @@ def main():
     GPIO.setboard(GPIO.H616)
     GPIO.setmode(GPIO.BOARD)
 
-    GPIO.setup(12, GPIO.OUT, GPIO.HIGH)
+    GPIO.setup(12, GPIO.OUT, GPIO.LOW)
 
     while True:
         sleep(1)
-        GPIO.output(12, GPIO.LOW)
+        GPIO.output(12, GPIO.HIGH)
         print("Relay on")
 
         sleep(1)
-        GPIO.output(12, GPIO.HIGH)
+        GPIO.output(12, GPIO.LOW)
         print("Relay off")
 
 
