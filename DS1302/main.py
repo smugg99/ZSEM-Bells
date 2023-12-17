24844270c7ee5e16eabb3df8fb872bfc8f88ec95
Dudus Jestem <dudak799@gmail.com>
2023-12-17 17:49:37 +0100

    test

--- DS1302/main.py
+++ DS1302/main.py
@@ -1,10 +1,10 @@
-from python.DS1302 import setup, get_ds_clock
+from python.DS1302 import setup, get_clock
 from datetime import datetime
 
 setup(clock_pin=7, data_pin=5, cs_pin=8)
 print(setup)
 
-year, mon, mday, hour, min, sec = get_ds_clock()
+year, mon, mday, hour, min, sec = get_clock()
 ds_datetime = datetime(year, mon, mday, hour, min, sec)
 
 print("DS1302 Clock:", ds_datetime)
