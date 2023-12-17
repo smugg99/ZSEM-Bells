186f07d1aaf3ffcf68600e80f9e0bcb25aef9f0a
Dudus Jestem <dudak799@gmail.com>
2023-12-18 00:29:44 +0100

    test

--- src/wrapper.py
+++ src/wrapper.py
@@ -100,6 +100,7 @@ def setup_gpio_pins() -> bool:
             GPIO.setmode(GPIO.BOARD)
 
             for pin in _outputs_config.values():
+                print(pin)
                 try:
                     GPIO.setup(pin, GPIO.OUT)
                 except Exception as e:
