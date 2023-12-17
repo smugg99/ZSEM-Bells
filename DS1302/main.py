5aef17f4fc6db8e0a869f6546b7e167d48037657
Dudus Jestem <dudak799@gmail.com>
2023-12-17 18:45:44 +0100

    test

--- DS1302/main.py
+++ DS1302/main.py
@@ -6,6 +6,7 @@ setup(clock_pin=7, data_pin=5, cs_pin=8)
 print("balls")
 
 year, mon, mday, hour, min, sec, deez = get_clock()
+print(year, mon, mday, hour, min, sec, deez)
 ds_datetime = datetime(year, mon, mday, hour, min, sec, deez)
 print("balls")
 print("DS1302 Clock:", ds_datetime)
